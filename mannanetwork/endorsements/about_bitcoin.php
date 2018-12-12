b565de0d596395c5b7ce3e9d8fc977ca623c96e4
Robert Lefebvre <robert@bungeebones.com>
2018-12-11 22:07:15 -0500

    Update about_bitcoin.php

--- mannanetwork/endorsements/about_bitcoin.php
+++ mannanetwork/endorsements/about_bitcoin.php
@@ -2,8 +2,8 @@
 
 <div id="openModal" class="modalWindow">
         <div class="modalHeader"><h1 align="center">About Bitcoin</h1></div>
-            <p align="left">Bitcoin is a new and revolutionary web technology that can be used for many things. In the case of our blogging club and monetization system we use it as the payment system. Any advertising fees that a web site chooses to incur to move their site into a more prominent position are paid using Bitcoin (unless the buyer has earned advertising credits and is using those instead). Also, all commissions paid to club members for their advertising sales are paid out in Bitcoin.</p>
-            <p  align="left">You can find a lot of information about Bitcoin on the web. It has been out now about 5 years and there are many excellent YouTube videos, many excellent web sites, and many excellent news articles.    
+            <p align="left">Bitcoin is a new and revolutionary web technology that can be used for many things. In the case of our blogging club and monetization system, we use it as the payment system. Any web site that chooses purchase a more prominent position use Bitcoin (unless the buyer has earned advertising credits and is using those instead) because all commissions are paid to club members (i.e. for their advertising sales) out in Bitcoin too.</p>
+            <p  align="left">You can find a lot of information about Bitcoin on the web. It has been out now about 10 years and there are many excellent YouTube videos, many excellent web sites, and many excellent news articles. The Manna Network uses the Bitcoin SV version.    
          
 </div>     
             <div class="clear"></div>
